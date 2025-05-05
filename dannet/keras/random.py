b0ec129cf680278d8498aa1d37da62ad9e98ccbc
DanisNone <gubajdullindanis@gmail.com>
2025-05-05 12:57:40 +0500

    .

--- dannet/keras/random.py
+++ dannet/keras/random.py
@@ -10,6 +10,7 @@ from keras.src.random.seed_generator import make_default_seed
 def dt_draw_seed(seed):
     seed = draw_seed(seed)
     if isinstance(seed, dt.core.TensorBase):
+        # it's bad.
         x, y = dt.eval(seed).numpy()
         x, y = int(x), int(y)
         return (x << 32) | y
@@ -57,7 +58,6 @@ def dropout(inputs, rate, noise_shape=None, seed=None):
     noise_shape = _get_concrete_noise_shape(inputs, noise_shape)
     mask = dt.random.uniform(shape=noise_shape, rng=rng, dtype='float32')
     mask = dt.less(mask, keep_prob)
-    mask = dt.broadcast_to(mask, inputs.shape)
-
+    
     res = inputs / keep_prob
-    return dt.where(mask, res, dt.zeros_like(res))
+    return res * mask
